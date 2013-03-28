39d5b827336d258fa0565f946a87baf225164f7f
knuthelgesen <knut.helgesen@bouvet.no>
2013-03-28 22:21:37 +0100

    Use better range calculation

--- src/main/java/no/plasmid/bird/TerrainTileManager.java
+++ src/main/java/no/plasmid/bird/TerrainTileManager.java
@@ -101,7 +101,8 @@ public class TerrainTileManager {
 		 * @return
 		 */
 		private int calculateRange(int x1, int x2, int z1, int z2) {
-			return Math.abs(x2 - x1) + Math.abs(z2 - z1);
+			return (int)(Math.sqrt((x2 - x1) * (x2 - x1) + (z2 - z1) * (z2 - z1))); 
+//			return Math.abs(x2 - x1) + Math.abs(z2 - z1);
 		}
 		
 		private int nextPow2(int value) {
